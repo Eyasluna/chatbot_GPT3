f127b08dbed7415616413f4297603dbbeaebc7aa
Yibo Fu <yibomus@gmail.com>
2024-05-14 22:18:20 -0400

    create respond model

--- website/models.py
+++ website/models.py
@@ -1,1 +1,8 @@
-c
+from . import db
+
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(20))
+    message = db.Column(db.String(500))
+    message_type = db.Column(db.String(100))
