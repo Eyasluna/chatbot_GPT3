ac7a0370d4a49218dd2b8f2ee6afe07c37cd3b6d
Yibo Fu <yibomus@gmail.com>
2024-06-01 22:28:09 -0400

    reslove openai version issue

--- website/models.py
+++ website/models.py
@@ -1,8 +1,8 @@
-from . import db
+from website import db
 
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(20))
-    message = db.Column(db.String(500))
-    message_type = db.Column(db.String(100))
+    time = db.Column(db.DateTime, nullable=False)
+    message = db.Column(db.String(200), nullable=False)
+    message_type = db.Column(db.String(50), nullable=False)
--- website/routes.py
+++ website/routes.py
@@ -1,8 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from openai import OpenAI
 
+client = OpenAI(api_key='sk-chatbot-wCAWYUoRkTpsO2F66561T3BlbkFJq4ypl1aGBkfwMkoWTlc8')
 routes = Blueprint('routes', __name__)
+history_list = []
 
 
 @routes.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("base.html")
+    from website.models import Result
+
+    if request.method == "GET":
+        query = request.args.get('query')
+        if query == "" or query is None:
+            return render_template("response_view.html")
+
+        if query:
+            answer = ask(query)
+
+            data_list = []
+            history = Result.query.all()
+            query_message = Result(time="12:00", message=query, message_type="query")
+            response_message = Result(time="12:01", message=answer, message_type="response")
+            data_list.append(query_message)
+            data_list.append(response_message)
+            for item in history:
+                history_list.append(item)
+        return render_template("response_view.html", results=data_list)
+    else:
+        return render_template("history.html", results=history_list)
+
+
+def ask(question, chat_log=None):
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question}
+        ]
+    )
+    answer = response.choices[0].message.content
+    return answer
