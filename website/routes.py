bb5e0c75899dc4ab4720ed009e7d8eb2d0d3d39c
Yibo Fu <yibomus@gmail.com>
2024-06-01 22:46:42 -0400

    history page resloved

--- website/routes.py
+++ website/routes.py
@@ -14,25 +14,20 @@ def home():
         query = request.args.get('query')
         if query == "" or query is None:
             return render_template("response_view.html")
-
-        if query:
-            answer = ask(query)
-
-            data_list = []
-            history = Result.query.all()
-            query_message = Result(time="12:00", message=query, message_type="query")
-            response_message = Result(time="12:01", message=answer, message_type="response")
-            data_list.append(query_message)
-            data_list.append(response_message)
-            for item in history:
-                history_list.append(item)
+        answer = ask(query)
+        data_list = []
+        query_message = Result(time="12:00", message=query, message_type="query")
+        response_message = Result(time="12:01", message=answer, message_type="response")
+        data_list.append(query_message)
+        data_list.append(response_message)
+        history_list.append(query_message)
+        history_list.append(response_message)
         return render_template("response_view.html", results=data_list)
     else:
         return render_template("history.html", results=history_list)
 
 
-def ask(question, chat_log=None):
-    prompt = f'{chat_log}Human: {question}\nAI:'
+def ask(question):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
